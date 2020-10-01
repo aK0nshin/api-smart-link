318a12268c237794bf4923762476a597d83e94aa
Коншин Александр Алексеевич <konshin_a@tass.ru>
2020-10-01 08:32:30 +0300

    Add pages table

--- api_smart_link/database/tables.py
+++ api_smart_link/database/tables.py
@@ -1,14 +1,26 @@
-from sqlalchemy import MetaData, Table, Column, BigInteger, DateTime, String, Boolean
+from datetime import datetime
+
+from sqlalchemy import MetaData, Table, Column, BigInteger, DateTime, String, ForeignKey, JSON, text, func
 
 db_meta = MetaData()
 
-notes = Table(
+users = Table(
     "users",
     db_meta,
     Column("id", BigInteger, primary_key=True),
-    Column("created_at", DateTime, nullable=True),
-    Column("updated_at", DateTime, nullable=True),
+    Column("created_at", DateTime, default=func.now(), nullable=True),
+    Column("updated_at", DateTime, onupdate=func.now(), nullable=True),
     Column("name", String(length=128)),
     Column("email", String(length=128)),
     Column("password", String(length=256)),
+)
+
+pages = Table(
+    "pages",
+    db_meta,
+    Column("id", BigInteger, primary_key=True),
+    Column("created_at", DateTime, default=func.now(), nullable=True),
+    Column("updated_at", DateTime, onupdate=func.now(), nullable=True),
+    Column('user_id', BigInteger, ForeignKey("users.id"), nullable=False),
+    Column("content", JSON, default=text("'{}'")),
 )
